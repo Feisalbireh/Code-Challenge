cca673aad96c61ca59eaa3384cd05326d5cf32de
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-11-27 20:10:47 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function speedDetector(speed){
+const speedLimit= 70
+const speedRate= 5
+
+if (speed < speedLimit){
+    return "Ok"
+}
+
+const speedDiffernce = speed-speedLimit 
+
+let demPoints = 0
+for(let i = 0; i < speedDiffernce; i+=speedRate){
+    demPoints += 1
+
+    if(demeritPoints > 12){
+        return "License suspended"
+    }
+}
+return `Points: ${demPoints}`
+}
---
+++ index2.js
@@ -0,0 +1,24 @@
+
+Grade
+let score = 100
+let Grade
+
+if(score >= 90){
+
+    Grade = `A`
+}
+else if(score >= 80){
+    Grade = `B`
+}
+else if(score => 70){
+    Grade = `C`
+}
+else if(score >= 60){
+    Grade = `D`
+}
+else{
+    Grade =`E`
+}
+
+
+
