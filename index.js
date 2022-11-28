00a620ae939988b4ad588d3c14adb8fecd428fb7
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-11-28 15:45:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+function speedDetector(speed) {
+  const speedLimit = 70;
+  const speedRate = 5;
 
-function speedDetector(speed){
-const speedLimit= 70
-const speedRate= 5
+  if (speed < speedLimit) {
+    return "Ok";
+  }
 
-if (speed < speedLimit){
-    return "Ok"
-}
+  const speedDiffernce = speed - speedLimit;
 
-const speedDiffernce = speed-speedLimit 
+  let demPoints = 0;
+  for (let i = 0; i < speedDiffernce; i += speedRate) {
+    demPoints += 1;
 
-let demPoints = 0
-for(let i = 0; i < speedDiffernce; i+=speedRate){
-    demPoints += 1
-
-    if(demPoints > 12){
-        return "License suspended"
+    if (demPoints > 12) {
+      return "License suspended";
     }
+  }
+  return `Points: ${demPoints}`;
 }
-return `Points: ${demPoints}`
-}
+
+console.log(speedDetector(170));
--- index2.js
+++ index2.js
@@ -1,24 +1,19 @@
-
-Grade
-let score = 100
-let Grade
-
-if(score >= 90){
-
-    Grade = `A`
+const StudentGrading = require("prompt-sync");
+const prompt = StudentGrading();
+function generator() {
+  let grade = prompt("enter your percentage:");
+  if (grade <= 100 && grade > 79) {
+    console.log("Grade is A");
+  } else if (grade <= 79 && grade > 59) {
+    console.log("Grade is B");
+  } else if (grade <= 59 && grade > 49) {
+    console.log("Grade is C");
+  } else if (grade <= 49 && grade > 39) {
+    console.log("Grade is D");
+  } else if (grade <= 39 && grade >= 0) {
+    console.log("Grade is E");
+  } else {
+    console.log("enter the correct percentage");
+  }
 }
-else if(score >= 80){
-    Grade = `B`
-}
-else if(score => 70){
-    Grade = `C`
-}
-else if(score >= 60){
-    Grade = `D`
-}
-else{
-    Grade =`E`
-}
-score(56)
-
-
+generator();
